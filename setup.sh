cbca8e2d5dd3d49bf2df0e163195d1b4b0c4d869
Martin, Chen <haochuan.z.chen@intel.com>
2026-05-07 14:53:58 +0800

    init

---
+++ setup.sh
@@ -0,0 +1,6 @@
+export SOARM_ID=my_awesome_follower_arm
+export DETECTION_WEIGHTS=~/openclaw/smi/yolo11n.pt
+export DETECTION_SOURCE=38
+export CONDA_BIN=/home/intel/miniconda3/condabin/conda
+#export DETECTION_CONF=0.25
+
